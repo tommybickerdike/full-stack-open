25bbacfa6b513bf9d2a569886fb8c87f0e7ebcef
Tommy <tommy@musicglue.com>
2021-10-01 12:01:29 +0100

    1.5

--- part1/src/Total.js
+++ part1/src/Total.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 const Total = (props) => {
-	console.log(props[0]);
 	return (
 		<footer>
 			<p>
-				Number of exercises &nbsp;
+				Number of exercises&nbsp;
 				{props.parts[0].exercises +
 					props.parts[1].exercises +
 					props.parts[2].exercises}
