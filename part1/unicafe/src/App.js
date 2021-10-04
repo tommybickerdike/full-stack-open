493e81712436410188a66cf98d63c3efa6fe7bcc
Tommy <tommy@musicglue.com>
2021-10-04 14:57:15 +0100

    1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 
 const StatisticLine = ({ text, value }) => {
-	return (
-		<p>
-			{text} {value}
-		</p>
-	);
+	if (value) {
+		return (
+			<tr>
+				<td>{text}</td>
+				<td>{value}</td>
+			</tr>
+		);
+	} else {
+		return (
+			<tr>
+				<td>{text}</td>
+				<td>?</td>
+			</tr>
+		);
+	}
 };
 
 const Statistics = (props) => {
 	const values = props.values;
 	if (values.total) {
 		return (
-			<div>
-				<StatisticLine text={"Good"} value={values.good} />
-				<StatisticLine text={"Neutral"} value={values.neutral} />
-				<StatisticLine text={"Bad"} value={values.bad} />
-				<StatisticLine text={"All"} value={values.total} />
-				<StatisticLine text={"Average"} value={values.average} />
-				<StatisticLine text={"Positive"} value={values.positive} />
-			</div>
+			<table>
+				<tbody>
+					<StatisticLine text={"Good"} value={values.good} />
+					<StatisticLine text={"Neutral"} value={values.neutral} />
+					<StatisticLine text={"Bad"} value={values.bad} />
+					<StatisticLine text={"All"} value={values.total} />
+					<StatisticLine text={"Average"} value={values.average} />
+					<StatisticLine text={"Positive"} value={`${values.positive}%`} />
+				</tbody>
+			</table>
 		);
 	} else {
 		return <p>No feedback given</p>;
