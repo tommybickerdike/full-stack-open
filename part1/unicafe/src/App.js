1f25766f55cb14b85ea1794bbb3b4842f55c4afa
Tommy <tommy@musicglue.com>
2021-10-04 13:29:31 +0100

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,29 +1,55 @@
 import React, { useState } from "react";
 
 const App = () => {
-	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const [total, setTotal] = useState(0);
+	const [score, setScore] = useState(0);
+	const [average, setAverage] = useState(0);
+	const [positive, setPositive] = useState(0);
 
 	const Button = ({ text, handleClick }) => {
 		return <button onClick={handleClick}>{text}</button>;
 	};
 
-	const increaseGood = () => setGood(good + 1);
-	const increaseNeutral = () => setNeutral(neutral + 1);
-	const increaseBad = () => setBad(bad + 1);
+	const clickGood = () => {
+		setGood(good + 1);
+		setTotal(total + 1);
+		setScore(score + 1);
+		setAverage(score / total);
+		setPositive((good / total) * 100);
+	};
+
+	const clickNeutral = () => {
+		setNeutral(neutral + 1);
+		setTotal(total + 1);
+		setScore(score);
+		setAverage(score / total);
+		setPositive((good / total) * 100);
+	};
+
+	const clickBad = () => {
+		setBad(bad + 1);
+		setTotal(total + 1);
+		setScore(score - 1);
+		setAverage(score / total);
+		setPositive((good / total) * 100);
+	};
 
 	return (
 		<main>
 			<h1>give feedback</h1>
-			<Button handleClick={increaseGood} text={"Good"} />
-			<Button handleClick={increaseNeutral} text={"Neutral"} />
-			<Button handleClick={increaseBad} text={"Bad"} />
+			<Button handleClick={clickGood} text={"Good"} />
+			<Button handleClick={clickNeutral} text={"Neutral"} />
+			<Button handleClick={clickBad} text={"Bad"} />
 			<h1>statistics</h1>
 			<p>Good {good}</p>
 			<p>Neutral {neutral}</p>
 			<p>Bad {bad}</p>
+			<p>All {total}</p>
+			<p>Average {average}</p>
+			<p>Positive {positive}%</p>
 		</main>
 	);
 };
