1063639ea34875d4619382d62429413b7edba954
Tommy <tommy@musicglue.com>
2021-10-06 16:07:27 +0100

    2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,10 @@ import React, { useState } from "react";
 import Name from "./components/Name";
 
 const App = () => {
-	console.log("app");
 	const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
 	const [newName, setNewName] = useState("");
 
-	const addName = (event) => {
-		event.preventDefault();
+	const addName = () => {
 		const nameObject = {
 			name: newName,
 			id: persons.length + 1,
@@ -21,10 +19,20 @@ const App = () => {
 		setNewName(event.target.value);
 	};
 
+	const checkExists = (event) => {
+		event.preventDefault();
+
+		const currentList = persons.map((person) => person.name);
+
+		currentList.includes(newName)
+			? alert(`${newName} is already added to the phonebook`)
+			: addName();
+	};
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form onSubmit={addName}>
+			<form onSubmit={checkExists}>
 				<input value={newName} onChange={updateName} />
 				<button type="submit">add</button>
 			</form>
