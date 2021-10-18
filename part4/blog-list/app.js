3467460ce6e81c3e0d7ccbe2073dd5ca666dcc33
Tommy <tommy@musicglue.com>
2021-10-18 11:32:32 +0100

    4.2

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,5 +1,6 @@
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blog");
 const express = require("express");
 const app = express();
@@ -18,7 +19,13 @@ mongoose
 	});
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
+app.use(middleware.requestLogger);
+
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
