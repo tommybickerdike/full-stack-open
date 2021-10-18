b7ad2b7deb9a26e4980b159dec2c5c01630bf862
Tommy <tommy@musicglue.com>
2021-10-18 11:27:38 +0100

    4.2 wip

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,18 +1,10 @@
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const blogsRouter = require("./controllers/blog");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -27,24 +19,6 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs);
-	});
-});
-
-app.post("/api/blogs", (request, response) => {
-	const blog = new Blog(request.body);
-
-	blog
-		.save()
-		.then((result) => {
-			response.status(201).json(result);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-});
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
