28399dfb3a90798341db8cfa28c9dbf297c7e536
Tommy <tommy@musicglue.com>
2021-10-20 18:02:32 +0100

    4.20

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -4,24 +4,14 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-const getTokenFrom = (request) => {
-	const authorization = request.get("authorization");
-	console.log(authorization);
-	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-		return authorization.substring(7);
-	}
-	return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
 	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 	response.json(blogs.map((blog) => blog.toJSON()));
 });
 
 blogsRouter.post("/", async (request, response) => {
-	const token = getTokenFrom(request);
-	const decodedToken = jwt.verify(token, config.SECRET);
-	if (!token || !decodedToken.id) {
+	const decodedToken = jwt.verify(request.token, config.SECRET);
+	if (!request.token || !decodedToken.id) {
 		return response.status(401).json({ error: "token missing or invalid" });
 	}
 	const user = await User.findById(decodedToken.id);
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -12,6 +12,15 @@ const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: "unknown endpoint" });
 };
 
+const tokenExtractor = (request, response, next) => {
+	const authorization = request.get("authorization");
+	logger.info("Auth:  ", authorization);
+	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+		request.token = authorization.substring(7);
+	}
+	next();
+};
+
 const errorHandler = (error, request, response, next) => {
 	logger.error(error.message);
 
@@ -19,6 +28,8 @@ const errorHandler = (error, request, response, next) => {
 		return response.status(400).send({ error: "malformatted id" });
 	} else if (error.name === "ValidationError") {
 		return response.status(400).json({ error: error.message });
+	} else if (error.name === "JsonWebTokenError") {
+		return response.status(401).json({ error: "invalid token" });
 	}
 
 	next(error);
@@ -28,4 +39,5 @@ module.exports = {
 	requestLogger,
 	unknownEndpoint,
 	errorHandler,
+	tokenExtractor,
 };
