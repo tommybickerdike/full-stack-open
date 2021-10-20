cc543d4014b9395f0cc0a05494fb6a56a989d98e
Tommy <tommy@musicglue.com>
2021-10-20 15:11:27 +0100

    4.18

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -7,6 +7,7 @@ const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blog");
 const usersRouter = require("./controllers/user");
+const loginRouter = require("./controllers/login");
 const mongoose = require("mongoose");
 
 logger.info("connecting to", config.MONGODB_URI);
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
