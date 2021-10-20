0d7a51ddc11784323ba6929979b88941db209513
Tommy <tommy@musicglue.com>
2021-10-20 14:38:43 +0100

    4.17:

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,14 +1,27 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-	const blogs = await Blog.find({});
+	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 	response.json(blogs.map((blog) => blog.toJSON()));
 });
 
 blogsRouter.post("/", async (request, response) => {
-	const blog = new Blog(request.body);
+	const user = await User.find({});
+	const targetUser = user[0];
+
+	const blog = new Blog({
+		title: request.body.title,
+		author: request.body.author,
+		url: request.body.url,
+		user: targetUser.id,
+	});
+
 	const savedBlog = await blog.save();
+	targetUser.blogs = targetUser.blogs.concat(savedBlog._id);
+	await targetUser.save();
+
 	response.status(201).json(savedBlog.toJSON()).end();
 });
 
--- part4/blog-list/models/user.js
+++ part4/blog-list/models/user.js
@@ -4,6 +4,13 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true, minlength: 3 },
 	name: { type: String, required: true },
+	passwordHash: String,
+	blogs: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Blog",
+		},
+	],
 });
 
 userSchema.set("toJSON", {
