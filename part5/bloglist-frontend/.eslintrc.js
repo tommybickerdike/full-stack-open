94472dfae9acf93d400977e6c29265f9673e5d58
Tommy <tommy@musicglue.com>
2021-10-29 19:12:18 +0100

    5.17

--- part5/bloglist-frontend/.eslintrc.js
+++ part5/bloglist-frontend/.eslintrc.js
@@ -1,38 +1,22 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
+	env: {
+		browser: true,
+		es2021: true,
+		"cypress/globals": true,
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended"
-	],
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true
+	extends: ["eslint:recommended", "plugin:react/recommended"],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
 		},
-		"ecmaVersion": 12,
-		"sourceType": "module"
+		ecmaVersion: 12,
+		sourceType: "module",
 	},
-	"plugins": [
-		"react"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+	plugins: ["react", "jest", "cypress"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
 };
