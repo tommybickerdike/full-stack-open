156b187aa79ea26961f4f73ce92290b3398756d2
Tommy <tommy@musicglue.com>
2021-11-05 14:27:25 +0000

    7.4

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -6,6 +6,7 @@ import {
 	useRouteMatch,
 	useHistory,
 } from "react-router-dom";
+import { useField } from "./hooks";
 
 const Menu = () => {
 	const padding = {
@@ -93,22 +94,24 @@ const Footer = () => (
 const Notification = ({ message }) => (message ? <p>{message}</p> : "");
 
 const CreateNew = ({ addNew, setNotification }) => {
-	const [content, setContent] = useState("");
-	const [author, setAuthor] = useState("");
-	const [info, setInfo] = useState("");
+	const content = useField("text");
+	const author = useField("text");
+	const info = useField("text");
+
 	const history = useHistory();
 	let notificationTimer;
 
 	const handleSubmit = (e) => {
+		debugger;
 		e.preventDefault();
 		addNew({
-			content,
-			author,
-			info,
+			content: content.value,
+			author: author.value,
+			info: info.value,
 			votes: 0,
 		});
 		history.push("/");
-		setNotification(`a new anecdote '${content}' created!`);
+		setNotification(`a new anecdote '${content.value}' created!`);
 		clearTimeout(notificationTimer);
 		notificationTimer = setTimeout(() => {
 			setNotification("");
@@ -121,27 +124,15 @@ const CreateNew = ({ addNew, setNotification }) => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input
-						name="content"
-						value={content}
-						onChange={(e) => setContent(e.target.value)}
-					/>
+					<input {...content} />
 				</div>
 				<div>
 					author
-					<input
-						name="author"
-						value={author}
-						onChange={(e) => setAuthor(e.target.value)}
-					/>
+					<input {...author} />
 				</div>
 				<div>
 					url for more info
-					<input
-						name="info"
-						value={info}
-						onChange={(e) => setInfo(e.target.value)}
-					/>
+					<input {...info} />
 				</div>
 				<button>create</button>
 			</form>
