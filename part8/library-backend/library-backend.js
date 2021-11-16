cdaec6e79f712024861611efb664af698a6e1d94
Tommy <tommy@musicglue.com>
2021-11-16 11:52:35 +0000

    8.2

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -104,6 +104,7 @@ const typeDefs = gql`
 	type Query {
 		authorCount: Int!
 		bookCount: Int!
+		allBooks: [Book!]!
 	}
 `;
 
@@ -111,6 +112,7 @@ const resolvers = {
 	Query: {
 		authorCount: () => authors.length,
 		bookCount: () => books.length,
+		allBooks: () => books,
 	},
 };
 
