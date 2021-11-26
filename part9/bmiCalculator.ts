085a754343daeb3fcd4c6665e431156d00940580
Tommy <tommy@musicglue.com>
2021-11-26 18:35:54 +0000

    9.5 wip

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,17 +1,31 @@
-const h: number = Number(process.argv[2]);
-const w: number = Number(process.argv[3]);
-
-function calculateBmi(height: number, weight: number): string {
-	const BMI = (weight / height / height) * 10000;
-	if (BMI < 18.5) {
-		return "Underweight";
-	} else if (BMI < 25) {
-		return "Normal (healthy weight)";
-	} else if (BMI < 30) {
-		return "Overweight";
-	} else {
-		return "Obese";
-	}
+interface BMI {
+	height: number;
+	weight: number;
+	bmi: string;
 }
 
-console.log(calculateBmi(h, w));
+export default function calculateBmi(height: number, weight: number): BMI {
+	// CANNOT DO THIS AS ERROR VALUE CANNOT BE ASSIGNED TO BMI
+	// if (height <= 0 || weight <= 0) {
+	// 	return { error: "malformatted parameters" };
+	// }
+
+	const obj = {
+		height,
+		weight,
+		bmi: "",
+	};
+
+	const BMI = (weight / height / height) * 10000;
+	if (BMI < 18.5) {
+		obj.bmi = "Underweight";
+	} else if (BMI < 25) {
+		obj.bmi = "Normal (healthy weight)";
+	} else if (BMI < 30) {
+		obj.bmi = "Overweight";
+	} else {
+		obj.bmi = "Obese";
+	}
+
+	return obj;
+}
--- part9/index.ts
+++ part9/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
 const app = express();
 
-app.get("/hello", (req, res) => {
-	console.log(req.method, req.url, 200);
+app.get("/hello", (_req, res) => {
 	res.send("Hello Full Stack!");
 });
 
-const PORT = 3003;
+app.get("/bmi", (req, res) => {
+	const height = Number(req.query.height);
+	const weight = Number(req.query.weight);
+
+	res.send(calculateBmi(height, weight));
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
